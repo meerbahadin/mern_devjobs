3abbc2a03bbb9ebe38e786c7f62d39d2a00feefd
meer bahadin <you@example.com>
2020-07-30 16:59:13 +0300

    make it better

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const axios = require('axios');
+const path = require('path');
 
 app.use(cors());
 
 app.use(express.urlencoded());
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 app.get('/api/jobs', async (req, res) => {
   console.log(req.query);
@@ -15,6 +27,8 @@ app.get('/api/jobs', async (req, res) => {
   });
   res.json(response.data);
 });
+
+
 
 app.listen(5000, () => {
   console.log('App listening on port 5000');
